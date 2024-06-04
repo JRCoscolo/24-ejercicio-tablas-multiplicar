8b4a387df3e63fe0b30cc6de4c2a305999c367a9
EQ11A1\Juan Ra <coscolinguada@gmail.com>
2024-06-04 13:59:40 +0200

    Paso 1 (no funciona bien)

--- 24-ejercicio-tablas-multiplicar.js
+++ 24-ejercicio-tablas-multiplicar.js
@@ -1,1 +1,17 @@
+let tabla = 1
+let num = 1
+let rest = null
+
+while (tabla <= 10) {
+    while (num <= 10) {
+        rest = tabla * num
+        console.log(rest)
+        num++
+    }
+    num-10
+    tabla++
+}
+
+
+
 
